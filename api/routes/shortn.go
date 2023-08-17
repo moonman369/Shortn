6b97d3ab9a4e99ad5e63d9e64e5ef206633c4bad
Ayan Maiti <mightyayan369@gmail.com>
2023-08-17 13:19:11 +0000

    Implement custom short url v system generated unique id decision tree

--- api/routes/shortn.go
+++ api/routes/shortn.go
@@ -8,6 +8,7 @@ import (
 	"github.com/asaskevich/govalidator"
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 	"github.com/moonman369/Shortn/database"
 	"github.com/moonman369/Shortn/helpers"
 )
@@ -60,6 +61,30 @@ func ShortnURL(c *fiber.Ctx) error {
 
 	// enforce https, SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ := r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "Custom short is already in use"})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Unable to connect to server"})
+	}
 
 	r2.Decr(database.Ctx, c.IP())
 

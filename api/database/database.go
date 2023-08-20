7e1704617439149fb71d749d15990e843510dff4
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-08-20 12:11:39 +0530

    Restart docker containers

--- api/database/database.go
+++ api/database/database.go
@@ -2,19 +2,32 @@ package database
 
 import (
 	"context"
-	"os"
+	"fmt"
+	"time"
 
-	"github.com/go-redis/redis/v8"
+	"github.com/moonman369/Shortn/errorhandler"
+	"github.com/redis/go-redis/v9"
 )
 
 var Ctx = context.Background()
 
 func CreateClient(dbNo int) *redis.Client {
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr: os.Getenv("DB_ADDR"),
-		Password: os.Getenv("DB_PASSWORD"),
-		DB: dbNo,
+		Addr:     "redis:6379", // os.Getenv("DB_ADDRESS"),
+		Password: "",           // os.Getenv("DB_PASSWORD"),
+		DB:       0,
+		Protocol: 3,
 	})
+
+	ping, err := rdb.Ping(Ctx).Result()
+	if err != nil {
+		errorhandler.ErrorHandler(err)
+	}
+	rdb.Set(Ctx, "test-key", "test-val", 3*time.Minute)
+	fmt.Println(rdb.Get(Ctx, "test-key").Result())
+
+	fmt.Println(ping)
 
 	return rdb
 }
